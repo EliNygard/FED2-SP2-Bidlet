7e80dce60a677e3b9b1220c60c44c0bf962f5499
EliNygard <eli.nygard@gmail.com>
2024-11-20 19:31:35 +0100

    removed tailwind plugin from eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -12,13 +12,13 @@ export default [
     languageOptions: {
       globals: {
         ...globals.browser,
-        describe: true,   // Used for grouping tests
-        test: true,       // Used to create tests
-        it: true,        // Alternative way to create tests
-        expect: true,    // Used for test assertions
-        require: true,   // Used in Node.js files like Tailwind config
-        module: true,    // Used in Node.js files like Tailwind config
-        process: true,   // Used for environment variables later
+        describe: true, // Used for grouping tests
+        test: true, // Used to create tests
+        it: true, // Alternative way to create tests
+        expect: true, // Used for test assertions
+        require: true, // Used in Node.js files like Tailwind config
+        module: true, // Used in Node.js files like Tailwind config
+        process: true, // Used for environment variables later
       },
     },
   },
