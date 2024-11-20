cae154bd0440fe9f0670de20bf97c1e65c454ab8
EliNygard <eli.nygard@gmail.com>
2024-11-20 17:54:13 +0100

    updates colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
+  content: ["./**/*.{html,js,ts}", "!./node_modules/**/*"],
   theme: {
     extend: {
       fontFamily: {
@@ -8,25 +8,24 @@ export default {
         body: ["Inter", "sans-serif"],
       },
       fontSize: {
-        TwoRem: "2rem" 
+        TwoRem: "2rem",
       },
       colors: {
         brand: {
-          light:"373737",
-          default: "F5F5F5",
-          dark: "1A1A18",
+          light: "#373737",
+          default: "#F5F5F5",
+          dark: "#1A1A18",
         },
         accent: {
-          default: "FFD274",
-          dark: "FFAD00",
+          default: "#FFD274",
+          dark: "#FFAD00",
         },
         alert: {
-          light: "FEE0E0",
-          dark: "C40000"
-        }
-      }
+          light: "#FEE0E0",
+          dark: "#C40000",
+        },
+      },
     },
   },
   plugins: [],
-}
-
+};
