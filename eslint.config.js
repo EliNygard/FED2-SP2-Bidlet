2f9706cd80288c8650d76d7377d00fb5d7c5eced
EliNygard <eli.nygard@gmail.com>
2024-11-20 18:14:58 +0100

    installs and configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    ignores: ["dist/"],
+  },
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        describe: true,   // Used for grouping tests
+        test: true,       // Used to create tests
+        it: true,        // Alternative way to create tests
+        expect: true,    // Used for test assertions
+        require: true,   // Used in Node.js files like Tailwind config
+        module: true,    // Used in Node.js files like Tailwind config
+        process: true,   // Used for environment variables later
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
