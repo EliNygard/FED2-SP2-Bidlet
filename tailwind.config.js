f11a4a5a3c01fc99691533cca7dea8cfa4548f05
EliNygard <eli.nygard@gmail.com>
2024-11-20 17:50:36 +0100

    adds styles from style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./**/*.{html,js,ts}","!./node_modules/**/*"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: ["Manrope", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+      fontSize: {
+        TwoRem: "2rem" 
+      },
+      colors: {
+        brand: {
+          light:"373737",
+          default: "F5F5F5",
+          dark: "1A1A18",
+        },
+        accent: {
+          default: "FFD274",
+          dark: "FFAD00",
+        },
+        alert: {
+          light: "FEE0E0",
+          dark: "C40000"
+        }
+      }
+    },
   },
   plugins: [],
 }
