42e091ce379bbea1aa5009d714cc37aa3b5a998e
EliNygard <eli.nygard@gmail.com>
2024-11-21 15:15:36 +0100

    markup

--- src/js/router/views/home.ts
+++ src/js/router/views/home.ts
@@ -1,6 +1,13 @@
 function initializePage():void {
-    console.log("home page");
-    
+    const page = document.getElementById("app")
+    if(page) {
+        const heading = document.createElement('h1');
+        heading.textContent = 'Hello, Home page!';
+        heading.className = "text-accent-default bg-brand-light"
+        page.appendChild(heading);
+    } else {
+        console.error("Could not display page");
+    }
 }
 
 initializePage()
