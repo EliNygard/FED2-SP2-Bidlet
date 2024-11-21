cc77df7d33ee9baa53dadf2ee6ed5fa82bf5502d
EliNygard <eli.nygard@gmail.com>
2024-11-21 20:34:04 +0100

    scaffolding header in html

--- src/js/router/views/home.ts
+++ src/js/router/views/home.ts
@@ -1,10 +1,14 @@
 function initializePage():void {
     const page = document.getElementById("app")
     if(page) {
-        const heading = document.createElement('h1');
-        heading.textContent = 'Hello, Home page!';
-        heading.className = "text-accent-default bg-brand-light"
-        page.appendChild(heading);
+        console.log("home page");
+
+        
+        
+        // const heading = document.createElement('h1');
+        // heading.textContent = 'Hello, Home page!';
+        // heading.className = "text-accent-default bg-brand-light"
+        // page.appendChild(heading);
     } else {
         console.error("Could not display page");
     }
