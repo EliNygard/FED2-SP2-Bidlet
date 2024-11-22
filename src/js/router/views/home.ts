a90f2756580739644858ed6a26dcc41a0a3f56d2
EliNygard <eli.nygard@gmail.com>
2024-11-22 14:34:39 +0100

    completes footer component

--- src/js/router/views/home.ts
+++ src/js/router/views/home.ts
@@ -1,10 +1,17 @@
 import '../../components/header-component.ts'
+import '../../components/footer-component.ts'
 
 function initializePage():void {
     const page = document.getElementById("app")
     if(page) {
         const header = document.createElement('header-component')
-        page.appendChild(header)
+        const main = document.createElement('main')
+        main.textContent = 'Main'
+        const footer = document.createElement('footer-component')
+        
+        page.append(header, main, footer)
+
+        
     } else {
         console.error("Could not display page");
     }
