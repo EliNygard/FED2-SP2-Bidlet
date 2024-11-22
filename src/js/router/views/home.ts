e41961a51544bd26469dc5887f90f7c87548c8cb
EliNygard <eli.nygard@gmail.com>
2024-11-22 13:57:45 +0100

    clean up code

--- src/js/router/views/home.ts
+++ src/js/router/views/home.ts
@@ -3,17 +3,8 @@ import '../../components/header-component.ts'
 function initializePage():void {
     const page = document.getElementById("app")
     if(page) {
-        console.log("home page");
         const header = document.createElement('header-component')
-        console.log(header);
         page.appendChild(header)
-        
-        
-        
-        // const heading = document.createElement('h1');
-        // heading.textContent = 'Hello, Home page!';
-        // heading.className = "text-accent-default bg-brand-light"
-        // page.appendChild(heading);
     } else {
         console.error("Could not display page");
     }
