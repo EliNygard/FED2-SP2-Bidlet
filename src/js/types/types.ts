1ad0d099fda5bf17d20f6d9f3664621156a22731
EliNygard <eli.nygard@gmail.com>
2024-11-27 15:26:51 +0100

    moves interfaces to sep file

---
+++ src/js/types/types.ts
@@ -0,0 +1,36 @@
+export interface Media {
+    url: string;
+    alt: string;
+  }
+  
+  export interface User {
+    name: string;
+    email: string;
+    bio?: string;
+    avatar?: Media;
+    banner?: Media;
+    wins?: string[];
+  }
+  
+  export interface Bid {
+    id: string;
+    amount: number;
+    bidder: User;
+    created: string;
+  }
+  
+  export interface Listing {
+    id?: string;
+    title: string;
+    description?: string;
+    tags?: string[];
+    media?: Media[];
+    created: string;
+    updated: string;
+    endsAt: string;
+    seller?: User;
+    bids?: Bid[];
+    _count?: {
+      bids: number;
+    };
+  }
