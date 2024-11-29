6287db234313b1c9ff35597aeb296c5a3657dfea
EliNygard <eli.nygard@gmail.com>
2024-11-29 14:11:09 +0100

    adds formatting on date

--- src/js/utilities/formatting.ts
+++ src/js/utilities/formatting.ts
@@ -28,3 +28,15 @@ export function formatDate(dateString: string): string {
 
     return `${day} ${month} ${year}`
 }
+
+export function formatDateAndTime(dateString: string): string {
+  const value = dateString
+  const date = new Date(value)
+  const year = date.getFullYear()
+  const month = date.getMonth()
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+
+  return `${day}.${month}.${year} - ${hour}:${minutes}`
+}
