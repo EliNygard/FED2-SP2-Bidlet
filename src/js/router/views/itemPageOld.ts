f83e270c716df1062323246b2a458a3ff4a136fb
EliNygard <eli.nygard@gmail.com>
2024-12-02 17:28:57 +0100

    comment out unused files

--- src/js/router/views/itemPageOld.ts
+++ src/js/router/views/itemPageOld.ts
@@ -1,77 +1,77 @@
-import api from "../../../api/instance.ts";
-import "../../components/header-component.ts";
-import "../../components/footer-component.ts";
-import "../../components/item-page-component.ts";
-import ItemPageComponent from "../../components/item-page-component.ts";
-import { getCurrentBid } from "../../utilities/getCurrentBid.ts";
+// import api from "../../../api/instance.ts";
+// import "../../components/header-component.ts";
+// import "../../components/footer-component.ts";
+// import "../../components/item-page-component.ts";
+// import ItemPageComponent from "../../components/item-page-component.ts";
+// import { getCurrentBid } from "../../utilities/getCurrentBid.ts";
 
-async function initializePage(): Promise<void> {
-  const page = document.getElementById("app");
-  console.log(page);
+// async function initializePage(): Promise<void> {
+//   const page = document.getElementById("app");
+//   console.log(page);
   
-  const parameterString = window.location.search;
-  const searchParameters = new URLSearchParams(parameterString);
-  const listingId = searchParameters.get("id");
-  if (page) {
-    const header = document.createElement("header-component");
-    const main = document.createElement("main");
-    const footer = document.createElement("footer-component");
+//   const parameterString = window.location.search;
+//   const searchParameters = new URLSearchParams(parameterString);
+//   const listingId = searchParameters.get("id");
+//   if (page) {
+//     const header = document.createElement("header-component");
+//     const main = document.createElement("main");
+//     const footer = document.createElement("footer-component");
 
     
-    try {
+//     try {
 
-        const listing = await api.listing.read(listingId);
-        console.log(listing);
-        const highestBid = getCurrentBid(listing)
+//         const listing = await api.listing.read(listingId);
+//         console.log(listing);
+//         const highestBid = getCurrentBid(listing)
                         
-        const listingItem = document.createElement("item-page-component") as ItemPageComponent
-        listingItem.listingId = listing.id
-        listingItem.itemImage = {
-            src: listing.media?.[0]?.url || "default-image-url",
-            alt: listing.media?.[0]?.alt || `Image of item for sale: ${listing.title}`
-        }
-        listingItem.title = listing.title
-        listingItem.seller = listing.seller.name
-        listingItem.currentBid = `Current bid: ${highestBid} kr`
-        listingItem.timeLeft = listing.endsAt
-        listingItem.endsAt = listing.endsAt
-        listingItem.description = listing.description
+//         const listingItem = document.createElement("item-page-component") as ItemPageComponent
+//         listingItem.listingId = listing.id
+//         listingItem.itemImage = {
+//             src: listing.media?.[0]?.url || "default-image-url",
+//             alt: listing.media?.[0]?.alt || `Image of item for sale: ${listing.title}`
+//         }
+//         listingItem.title = listing.title
+//         listingItem.seller = listing.seller.name
+//         listingItem.currentBid = `Current bid: ${highestBid} kr`
+//         listingItem.timeLeft = listing.endsAt
+//         listingItem.endsAt = listing.endsAt
+//         listingItem.description = listing.description
         
-        main.appendChild(listingItem)
-        console.log(listingItem);
+//         main.appendChild(listingItem)
+//         console.log(listingItem);
         
-        listingItem.addEventListener("renderComplete", function handleRenderComplete() {
-            console.trace("RenderComplete event triggered");
+//         listingItem.addEventListener("renderComplete", function handleRenderComplete() {
+//             console.trace("RenderComplete event triggered");
 
-        const tags = listing.tags
-        if (tags) {
-            listingItem.addTags(tags);
-            console.log(tags);
-        }
-        const bids = listing.bids
-        if (bids) {
-            listingItem.addBids(bids)
-            console.log(bids);
+//         const tags = listing.tags
+//         if (tags) {
+//             listingItem.addTags(tags);
+//             console.log(tags);
+//         }
+//         const bids = listing.bids
+//         if (bids) {
+//             listingItem.addBids(bids)
+//             console.log(bids);
             
-        }
+//         }
 
-        listingItem.removeEventListener("renderComplete", handleRenderComplete);
-    })
+//         listingItem.removeEventListener("renderComplete", handleRenderComplete);
+//     })
         
-    } catch (error) {
-        if (error instanceof Error) {
-            console.error(error.message);
+//     } catch (error) {
+//         if (error instanceof Error) {
+//             console.error(error.message);
             
-        } else {
-            console.error("Ups, an unknown error occurred. We are sorry about the trouble. Please try again.");
+//         } else {
+//             console.error("Ups, an unknown error occurred. We are sorry about the trouble. Please try again.");
             
-        }
+//         }
         
-    }
-    page.append(header, main, footer);
-  } else {
-    console.error("Could not load list item. Please try again.");
-  }
-}
+//     }
+//     page.append(header, main, footer);
+//   } else {
+//     console.error("Could not load list item. Please try again.");
+//   }
+// }
 
-initializePage();
+// initializePage();
--- src/js/router/views/itemPageOld2.ts
+++ src/js/router/views/itemPageOld2.ts
@@ -1,48 +1,48 @@
-import api from "../../../api/instance.ts";
-import "../../components/header-component.ts";
-import "../../components/footer-component.ts";
-import "../../components/item-page-component-new.ts";
-import { getCurrentBid } from "../../utilities/getCurrentBid";
-import UserProfile from "../../components/item-page-component-new.ts";
+// import api from "../../../api/instance.ts";
+// import "../../components/header-component.ts";
+// import "../../components/footer-component.ts";
+// import "../../components/item-page-component-new.ts";
+// import { getCurrentBid } from "../../utilities/getCurrentBid";
+// import UserProfile from "../../components/item-page-component-new.ts";
 
-async function initializePage() {
-  console.log("Item page");
-  const page = document.getElementById("app");
-  console.log(page);
-  const parameterString = window.location.search;
-  const searchParameters = new URLSearchParams(parameterString);
-  const listingId = searchParameters.get("id");
-  const userProfile = document.createElement("user-profile") as UserProfile
+// async function initializePage() {
+//   console.log("Item page");
+//   const page = document.getElementById("app");
+//   console.log(page);
+//   const parameterString = window.location.search;
+//   const searchParameters = new URLSearchParams(parameterString);
+//   const listingId = searchParameters.get("id");
+//   const userProfile = document.createElement("user-profile") as UserProfile
     
-  if (page) {
-    const header = document.createElement("header-component");
-    const main = document.createElement("main");
-    const footer = document.createElement("footer-component");
+//   if (page) {
+//     const header = document.createElement("header-component");
+//     const main = document.createElement("main");
+//     const footer = document.createElement("footer-component");
 
-    try {
-      const listing = await api.listing.read(listingId);
-      console.log(listing);
-      const highestBid = getCurrentBid(listing);
-      console.log(highestBid);
-      const { title, description } = listing
-      console.log(title, description);
+//     try {
+//       const listing = await api.listing.read(listingId);
+//       console.log(listing);
+//       const highestBid = getCurrentBid(listing);
+//       console.log(highestBid);
+//       const { title, description } = listing
+//       console.log(title, description);
 
-      await customElements.whenDefined('user-profile');
-      userProfile.updateProfile(title, description)
+//       await customElements.whenDefined('user-profile');
+//       userProfile.updateProfile(title, description)
       
-    } catch (error) {
-      console.error(error);
-    } finally {
-      console.log("finally");
-    }
+//     } catch (error) {
+//       console.error(error);
+//     } finally {
+//       console.log("finally");
+//     }
 
-    main.appendChild(userProfile)
-    page.append(header, main, footer)
-  } else {
-    console.error("Could not load list item. Please try again.");
+//     main.appendChild(userProfile)
+//     page.append(header, main, footer)
+//   } else {
+//     console.error("Could not load list item. Please try again.");
     
-  }
+//   }
 
-}
+// }
 
-initializePage();
+// initializePage();
