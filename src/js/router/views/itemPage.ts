c0f959047e13af09729db97dc1267b2857b7faec
EliNygard <eli.nygard@gmail.com>
2024-12-02 15:33:40 +0100

    adds loader

--- src/js/router/views/itemPage.ts
+++ src/js/router/views/itemPage.ts
@@ -1,13 +1,11 @@
 import api from "../../../api/instance.ts";
 import "../../components/header-component.ts";
 import "../../components/footer-component.ts";
-import { getCurrentBid } from "../../utilities/getCurrentBid.ts";
+import "../../components/loader-component.ts";
 import { displayItem } from "../../components/displayItem.ts";
 
 async function initializePage(): Promise<void> {
-  console.log("Item page!");
   const page = document.getElementById("app");
-  console.log(page);
   const parameterString = window.location.search;
   const searchParameters = new URLSearchParams(parameterString);
   const listingId = searchParameters.get("id");
@@ -16,31 +14,28 @@ async function initializePage(): Promise<void> {
     const header = document.createElement("header-component");
     const main = document.createElement("main");
     const footer = document.createElement("footer-component");
+    const loader = document.createElement("loader-component")
 
+    main.appendChild(loader)
+    page.append(header, main, footer);
     try {
       if (!listingId) {
         throw new Error("Listing ID is missing");
       }
 
       const listing = await api.listing.read(listingId);
-      console.log(listing);
-      const highestBid = getCurrentBid(listing);
-      console.log(highestBid);
-      const { title, description } = listing;
-      console.log(title, description);
-
       const item = await displayItem(listing);
-      main.appendChild(item);
 
-      page.append(header, main, footer);
+      main.appendChild(item);
     } catch (error) {
       if (error instanceof Error) {
         console.error(error.message);
       } else {
         console.error("An unknown error occurred");
+        // display error, create a component
       }
     } finally {
-      console.log("finally");
+      main.removeChild(loader)
     }
   } else {
     console.error("Could not load list item. Please try again.");
