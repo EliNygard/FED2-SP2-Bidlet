e124f474972a276b1ba2aafa0831e016df13855c
EliNygard <eli.nygard@gmail.com>
2024-12-03 14:56:03 +0100

    adds loader

--- src/js/router/views/search.ts
+++ src/js/router/views/search.ts
@@ -2,12 +2,15 @@ import "../../components/header-component.ts";
 import "../../components/footer-component.ts";
 import "../../components/search-form-component.ts";
 import "../../components/listing-card-component.ts";
+import "../../components/loader-component.ts";
 import api from "../../../api/instance.ts";
 import { populateListings } from "../../utilities/populateListings.ts";
+import { delay } from "../../utilities/delay.ts";
 
 async function initializePage(): Promise<void> {
   const page = document.getElementById("app");
   if (page) {
+    const loader = document.createElement("loader-component");
     const header = document.createElement("header-component");
     const main = document.createElement("main");
     const searchForm = document.createElement("search-form-component");
@@ -19,13 +22,19 @@ async function initializePage(): Promise<void> {
     listingsSection.className =
       "max-w-7xl py-8 px-5 mt-4 m-auto grid gap-3 justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch";
 
-    bgListingsSection.appendChild(listingsSection);
+    // bgListingsSection.appendChild(listingsSection);
+    main.append(searchForm, loader, bgListingsSection);
+    page.append(header, main, footer);
 
     try {
-        const listings = await api.listings.readAll("&_active=true&sort=endsAt&sortOrder=asc")
-        populateListings(listings, listingsSection)
+      await delay(1000);
+      const listings = await api.listings.readAll("&_active=true&sort=endsAt&sortOrder=asc");
+      bgListingsSection.appendChild(listingsSection);
+      populateListings(listings, listingsSection);
     } catch (error) {
-        console.error("Error fetching listings.", error);
+      console.error("Error fetching listings.", error);
+    } finally {
+      main.removeChild(loader);
     }
 
     // Get search query and display search result
@@ -34,25 +43,28 @@ async function initializePage(): Promise<void> {
       const query = searchEvent.detail.query;
       console.log("Search query:", query);
 
-      listingsSection.innerHTML = ""; 
+      listingsSection.innerHTML = "";
+
+      main.appendChild(loader);
 
       try {
+        await delay(1000);
         const searchResult = await api.listings.search(query);
         console.log(searchResult);
-        populateListings(searchResult, listingsSection)
+        bgListingsSection.appendChild(listingsSection);
+        populateListings(searchResult, listingsSection);
+
         if (searchResult.length === 0) {
-            const noResultsMessage = document.createElement("p");
-            noResultsMessage.textContent = "No listings found. Please try a new search.";
-            listingsSection.appendChild(noResultsMessage);
-          }
-          
+          const noResultsMessage = document.createElement("p");
+          noResultsMessage.textContent = "No listings found. Please try a new search.";
+          listingsSection.appendChild(noResultsMessage);
+        }
       } catch (error) {
         console.error("Error fetching search results.", error);
+      } finally {
+        main.removeChild(loader);
       }
     });
-
-    main.append(searchForm, bgListingsSection);
-    page.append(header, main, footer);
   } else {
     console.error("Could not display page");
   }
