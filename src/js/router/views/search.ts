27820ec6ba7ff50d30a3b5f4419b6e3298b5ecea
EliNygard <eli.nygard@gmail.com>
2024-12-03 19:35:40 +0100

    removes delay, refactor code to helper func

--- src/js/router/views/search.ts
+++ src/js/router/views/search.ts
@@ -5,7 +5,6 @@ import "../../components/listing-card-component.ts";
 import "../../components/loader-component.ts";
 import api from "../../../api/instance.ts";
 import { populateListings } from "../../utilities/populateListings.ts";
-import { delay } from "../../utilities/delay.ts";
 
 async function initializePage(): Promise<void> {
   const page = document.getElementById("app");
@@ -22,15 +21,13 @@ async function initializePage(): Promise<void> {
     listingsSection.className =
       "max-w-7xl py-8 px-5 mt-4 m-auto grid gap-3 justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch";
 
-    // bgListingsSection.appendChild(listingsSection);
-    main.append(searchForm, loader, bgListingsSection);
+    main.append(searchForm, loader);
     page.append(header, main, footer);
 
     try {
-      await delay(1000);
       const listings = await api.listings.readAll("&_active=true&sort=endsAt&sortOrder=asc");
       bgListingsSection.appendChild(listingsSection);
-      populateListings(listings, listingsSection);
+      populateListings(listings, listingsSection, bgListingsSection, main);
     } catch (error) {
       console.error("Error fetching listings.", error);
     } finally {
@@ -41,24 +38,14 @@ async function initializePage(): Promise<void> {
     searchForm.addEventListener("search", async (event: Event) => {
       const searchEvent = event as CustomEvent;
       const query = searchEvent.detail.query;
-      console.log("Search query:", query);
 
-      listingsSection.innerHTML = "";
-
-      main.appendChild(loader);
-
+      main.innerHTML = "";
+      
+      main.append(searchForm, loader);
+      
       try {
-        await delay(1000);
         const searchResult = await api.listings.search(query);
-        console.log(searchResult);
-        bgListingsSection.appendChild(listingsSection);
-        populateListings(searchResult, listingsSection);
-
-        if (searchResult.length === 0) {
-          const noResultsMessage = document.createElement("p");
-          noResultsMessage.textContent = "No listings found. Please try a new search.";
-          listingsSection.appendChild(noResultsMessage);
-        }
+        populateListings(searchResult, listingsSection, bgListingsSection, main);
       } catch (error) {
         console.error("Error fetching search results.", error);
       } finally {
