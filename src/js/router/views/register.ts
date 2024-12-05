b5d1f5d4bbe24a0eebf7b1f9560cd41c86bba993
EliNygard <eli.nygard@gmail.com>
2024-12-05 20:55:54 +0100

    append auth component and handle close button

--- src/js/router/views/register.ts
+++ src/js/router/views/register.ts
@@ -1,6 +1,23 @@
-function initializePage():void {
-    console.log("register account");
-    
+import "../../components/auth-component.ts";
+
+function initializePage(): void {
+  const page = document.getElementById("app");
+  if (page) {
+    const main = document.createElement("main");
+
+    const registerForm = document.createElement("auth-component");
+    registerForm.setAttribute("data-mode", "register");
+
+    main.appendChild(registerForm);
+    page.appendChild(main);
+
+    const closeButton = document.querySelector("#closeButton");
+    if (closeButton) {
+      closeButton.addEventListener("click", () => {
+        window.location.href = "./";
+      });
+    }
+  }
 }
 
-initializePage()
+initializePage();
