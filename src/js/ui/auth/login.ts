875ab631e1551ff2381b42e3d7c81b573d19b23b
EliNygard <eli.nygard@gmail.com>
2024-12-06 13:07:43 +0100

    adds loader and display error message

--- src/js/ui/auth/login.ts
+++ src/js/ui/auth/login.ts
@@ -1,30 +1,59 @@
-import api from "../../../api/instance.ts"
+import { ApiError } from "../../../api/error.ts";
+import api from "../../../api/instance.ts";
 import { LoginUser } from "../../types/types.ts";
+import { createErrorMessageElement } from "../errorHandling/createErrorMessage.ts";
+import "../../components/auth-loader-component.ts"
 
 export async function onLogin(event: Event) {
-    event.preventDefault()
-    const form = event.target as HTMLFormElement | null
-    if (!form) {
-        console.error("Login form not found. Please try again.");
-        return
-    }
-    const formData = new FormData(form)
-    const data = Object.fromEntries(formData.entries()) as unknown as LoginUser
-    console.log(data);
-    
+  event.preventDefault();
 
-    // loader
+  const form = event.target as HTMLFormElement | null;
+  if (!form) {
+    console.error("Login form not found. Please try again.");
+    return;
+  }
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries()) as unknown as LoginUser;
+  console.log(data);
+  const button = form.querySelector("button[type='submit']");
+  if (!button) {
+    console.error("Submit button not found");
+    return;
+  }
+  const loader = document.createElement("auth-loader-component");
+  button.textContent = "";
+  button.appendChild(loader);
 
-    try {
-        const { name } = await api.auth.login(data) 
-        window.location.href = `./profile?name=${name}`
-    } catch (error) {
-        console.error("Login failed:", error);
-        //display error as in Figma
-        alert(error)
-        // clear form
-    } finally {
-        // hide loader
+  try {
+    const { name } = await api.auth.login(data);
+    window.location.href = `./profile?name=${name}`;
+  } catch (error) {
+    if (error instanceof ApiError) {
+      console.error("Log in failed:", {
+        status: error.status,
+        statusText: error.statusText,
+        messages: error.messages,
+      });
+    } else {
+      console.error("Log in failed:", error);
     }
-    
+
+    const authComponent = document.querySelector("auth-component");
+    console.log(authComponent);
+
+    if (authComponent) {
+      const heading = authComponent.querySelector("h1");
+      const errorMessage = createErrorMessageElement(
+        error instanceof ApiError ? error.message : "An error occurred. Please try again.",
+      );
+      if (heading) {
+        heading.insertAdjacentElement("afterend", errorMessage);
+      } else {
+        authComponent.appendChild(errorMessage);
+      }
+    }
+  } finally {
+    button.removeChild(loader)
+    button.textContent = "Log in"
+  }
 }
--- src/js/ui/auth/register.ts
+++ src/js/ui/auth/register.ts
@@ -1,97 +1,58 @@
 import api from "../../../api/instance.ts";
 import { RegisterUser } from "../../types/types.ts";
 import "../../components/auth-component.ts";
+import "../../components/auth-loader-component.ts";
 import { ApiError } from "../../../api/error.ts";
+import { createErrorMessageElement } from "../errorHandling/createErrorMessage.ts";
+import { displayLoginComponent } from "../displayLoginComp.ts";
 
 export async function onRegister(event: Event) {
   event.preventDefault();
-  const form = event.target as HTMLFormElement | null;
 
+  const authComponent = document.querySelector("auth-component");
+  const form = event.target as HTMLFormElement | null;
   if (!form) {
     console.error("Register form not found. Please try again.");
     return;
   }
-
   const formData = new FormData(form);
   const data = Object.fromEntries(formData.entries()) as unknown as RegisterUser;
-
-  // loader
+  const button = form.querySelector("button[type='submit']");
+  if (!button) {
+    console.error("Submit button not found");
+    return;
+  }
+  const loader = document.createElement("auth-loader-component");
+  button.textContent = "";
+  button.appendChild(loader);
 
   try {
     await api.auth.register(data);
     displayLoginComponent();
   } catch (error) {
     if (error instanceof ApiError) {
-        console.error("Registration failed:", {
-          status: error.status,
-          statusText: error.statusText,
-          messages: error.messages,
-        });
-      } else {
-        console.error("Registration failed:", error);
-      }
+      console.error("Registration failed:", {
+        status: error.status,
+        statusText: error.statusText,
+        messages: error.messages,
+      });
+    } else {
+      console.error("Registration failed:", error);
+    }
 
-    const authComponent = document.querySelector("auth-component")
     if (authComponent) {
-        const heading = authComponent.querySelector("h1")
-        console.log(heading);
-        const errorMessage = createErrorMessageElement(
-            error instanceof ApiError ? error.message : "An error occured. Please try again"
-        )
-        if (heading) {
-            heading.insertAdjacentElement("afterend", errorMessage)
-        } else {
-            authComponent.appendChild(errorMessage)
-        }
-        
-    } 
-    // alert("Could not register user." + error.messages +". Please try again." || "An unknown error occurred. Please try again.")
+      const heading = authComponent.querySelector("h1");
+      const errorMessage = createErrorMessageElement(
+        error instanceof ApiError ? error.message : "An error occurred. Please try again",
+      );
+      if (heading) {
+        heading.insertAdjacentElement("afterend", errorMessage);
+      } else {
+        authComponent.appendChild(errorMessage);
+      }
+    }
   } finally {
-    // hide loader
+    button.removeChild(loader);
+    button.textContent = "Register";
   }
-}
-
-function createErrorMessageElement(message: string): HTMLElement {
-    const container = document.createElement("div")
-    container.className = "mt-9 border border-alert-dark bg-alert-light py-4 px-5 font-body text-base max-w-96 flex gap-6"
-
-    const errorText = document.createElement("p")
-    errorText.textContent = `${message}. Please try again.`
-    
-    const closeButton = document.createElement("button")
-    closeButton.className = "cursor-pointer"
-    closeButton.addEventListener("click", () => {
-        container.remove()
-    })
-
-    const icon = document.createElement("span")
-    icon.className = "fa-solid fa-x"
-
-    closeButton.appendChild(icon)
-    container.appendChild(errorText)
-    container.appendChild(closeButton)
-
-    return container
-}
-
-function displayLoginComponent() {
-  const page = document.getElementById("app");
-
-  if (!page) {
-    console.error("No page found. Please try again.");
-    return;
-  }
-
-  page.innerHTML = "";
-
-  const message = document.createElement("p");
-  message.textContent = "Registration successful! Please log in to continue.";
-  message.className = "";
-
-  page.appendChild(message);
-
-  const loginComponent = document.createElement("auth-component");
-  loginComponent.setAttribute("data-mode", "login");
-
-  page.appendChild(loginComponent);
 }
