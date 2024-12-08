2a948e44cdbd9b7a7341463a818ecc78425db0b6
EliNygard <eli.nygard@gmail.com>
2024-12-08 18:03:22 +0100

    adds header, main, footer

--- src/js/router/views/profile.ts
+++ src/js/router/views/profile.ts
@@ -1,10 +1,16 @@
-
-
-
+import "../../components/header-component.ts";
+import "../../components/footer-component.ts";
 
 function initializePage():void {
     console.log("profile page");
-    
+    const page = document.getElementById("app");
+  if (page) {
+    const header = document.createElement("header-component");
+    const main = document.createElement("main");
+    const footer = document.createElement("footer-component");
+
+    page.append(header, main, footer);
+}
 }
 
 initializePage()
