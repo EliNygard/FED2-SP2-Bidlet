d16c93745bb91d17748fb991d6598e0fb1a26f06
EliNygard <eli.nygard@gmail.com>
2024-12-09 15:45:17 +0100

    adds data to user card

--- src/js/components/user-card-component.ts
+++ src/js/components/user-card-component.ts
@@ -8,7 +8,6 @@ class UserCard extends HTMLElement {
 
   async connectedCallback() {
     const userName = this.getAttribute("user-name");
-    console.log(userName);
 
     if (userName) {
       try {
@@ -17,24 +16,67 @@ class UserCard extends HTMLElement {
         this.render(userData);
       } catch (error) {
         console.error(error);
-        this.render(null)
+        this.render(null);
       }
     }
   }
   render(userData: Profile | null) {
     if (userData) {
       this.innerHTML = `
-        <div class="user-card">
-        <h2>${userData.name}</h2>
-        <p>${userData.email}</p>
+        <div class="mx-auto">
+        <div class="max-w-4xl mx-auto px-6 mt-14">
+          <section class="flex">
+          <button id="profileImage" class="w-24 h-32">
+            <img class="rounded-md h-full object-cover" src="${userData.avatar?.url}" alt="">
+          </button>
+          <div class="font-body ml-4 flex flex-col items-start">
+            <h1 class="text-2xl mb-5">${userData.name}</h1>
+            <div class="flex flex-col items-center text-sm">
+              <p>${userData.credits}</p>
+              <p>credits</p>
+            </div>
+          </div>
+        </section>
+        
+        <div class="my-9">
+          <button class="btn btn-accent px-3 py-2 text-brand-dark text-xs">Log out</button>
         </div>
+  
+        <header class="font-body text-base md:text-lg flex justify-between max-w-sm">
+          <button class="hover:underline active:underline">My items</button>
+          <button class="hover:underline active:underline">My wins</button>
+          <button class="hover:underline active:underline">My bids</button>
+        </header>
+
+      </div>
+
+      <div class="bg-brand-default">
+
+        <div id="itemsContainer" class="max-w-4xl py-8 px-6 mt-4 m-auto grid gap-3 justify-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-stretch">
+          
+
+        </div>
+      </div>
+      
+    </div>
         `;
     } else {
       this.innerHTML = `
           <p>Failed to load user data</p>
           `;
     }
+    const itemsContainer = this.querySelector<HTMLElement>("#itemsContainer")
+    console.log(itemsContainer);
+    const myListings = userData?.listings
+    console.log(myListings);
+    myListings?.forEach((listing) => {
+      console.log(listing);
+      
+    })
+    
   }
+
+  
 }
 
 customElements.define("user-card-component", UserCard);
--- src/js/router/views/profile.ts
+++ src/js/router/views/profile.ts
@@ -2,25 +2,29 @@ import "../../components/header-component.ts";
 import "../../components/footer-component.ts";
 import "../../components/user-card-component.ts";
 import api from "../../../api/instance.ts";
+import { showLoader } from "../../utilities/showLoader.ts";
+import { hideLoader } from "../../utilities/hideLoader.ts";
 
 async function initializePage(): Promise<void> {
   const page = document.getElementById("app");
   if (page) {
-    const header = document.createElement("header-component");
-    const main = document.createElement("main");
-    const footer = document.createElement("footer-component");
+    showLoader(page);
+    try {
+      const userName = api.user.name;
 
-    const userName = api.user.name;
-    console.log(userName);
-    const userData = await api.profiles.singleProfile(userName);
-    console.log(userData);
+      const header = document.createElement("header-component");
+      const main = document.createElement("main");
+      const footer = document.createElement("footer-component");
+      const userCard = document.createElement("user-card-component");
+      userCard.setAttribute("user-name", userName);
 
-    const userCard = document.createElement("user-card-component");
-    userCard.setAttribute("user-name", userName);
-    console.log(userCard);
-
-    main.appendChild(userCard);
-    page.append(header, main, footer);
+      main.appendChild(userCard);
+      page.append(header, main, footer);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      hideLoader(page);
+    }
   }
 }
 
