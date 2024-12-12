02925f0b2b97982ef36b237ba31ed0d56c52d10c
EliNygard <eli.nygard@gmail.com>
2024-12-12 14:50:22 +0100

    feat: disable bid btn if no token, adds messages

--- src/js/router/views/itemPage.ts
+++ src/js/router/views/itemPage.ts
@@ -4,7 +4,6 @@ import "../../components/footer-component.ts";
 import { displayItem } from "../../components/displayItem.ts";
 import { showLoader } from "../../utilities/showLoader.ts";
 import { hideLoader } from "../../utilities/hideLoader.ts";
-import { delay } from "../../utilities/delay.ts";
 import { handleCarousel } from "../../utilities/handleCarousel.ts";
 
 async function initializePage(): Promise<void> {
@@ -12,26 +11,34 @@ async function initializePage(): Promise<void> {
   const parameterString = window.location.search;
   const searchParameters = new URLSearchParams(parameterString);
   const listingId = searchParameters.get("id");
-
+  const token = api.token
+  
   if (page) {
     showLoader(document.body);
     try {
-      await delay(1000);
       const header = document.createElement("header-component");
       const main = document.createElement("main");
       const footer = document.createElement("footer-component");
-
+      
       if (!listingId) {
         throw new Error("Listing ID is missing");
       }
-
+      
       const listing = await api.listing.read(listingId);
       const item = await displayItem(listing);
-
+      
       document.title = `Bidlet | ${listing.title}`;
-
+      
       page.append(header, main, footer);
       main.appendChild(item);
+
+      if (!token) {
+        const bidButton = document.getElementById("bid-btn")
+        if (bidButton) {
+          bidButton.className = "disabled: btn btn-big btn-disabled"
+          bidButton.title = "You must be logged in to make a bid"
+        }
+      }
 
       handleCarousel();
     } catch (error) {
