6d9d676daad3deac7a124f90111db331b2fd5bbd
EliNygard <eli.nygard@gmail.com>
2025-01-22 14:35:26 +0100

    Installs React, TS for React, plugin for Vite and configuring Vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
+import react from "@vitejs/plugin-react";
 
 dotenv.config();
 
 export default defineConfig({
+  plugins: [react()],
   define: {
     "process.env.API_KEY": JSON.stringify(process.env.API_KEY),
   },
