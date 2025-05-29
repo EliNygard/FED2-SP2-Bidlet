c115ae62533842fa00da273e6efff5a5218e1b89
EliNygard <eli.nygard@gmail.com>
2025-05-29 11:03:59 +0200

    Adds tests for wrong email or password on login

--- testse2e/login.test.js
+++ testse2e/login.test.js
@@ -7,7 +7,7 @@ test.describe("login", () => {
   const PASS_IN = `${DROPDOWN} input[name="password"]`;
   const SUBMIT_BTN = `${DROPDOWN} button:has-text("Log in")`;
   const LOGOUT_BTN = 'button:has-text("Log out")';
-  //   const MSG_CONT = `${DROPDOWN} #message-container`;
+  const MSG_CONT = `${DROPDOWN} [role="alert"]`;
 
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
@@ -20,5 +20,23 @@ test.describe("login", () => {
     await page.fill(PASS_IN, process.env.TEST_USER_PASSWORD);
     await page.click(SUBMIT_BTN);
     await expect(page.locator(LOGOUT_BTN)).toBeVisible();
+  });
+
+  test("wrong email shows error", async ({ page }) => {
+    await page.click(LOGIN_BTN);
+    await page.fill(EMAIL_IN, "wrong-email@stud.noroff.no");
+    await page.fill(PASS_IN, process.env.TEST_USER_PASSWORD);
+    await page.click(SUBMIT_BTN);
+    await expect(page.locator(MSG_CONT)).toBeVisible();
+    await expect(page.locator(MSG_CONT)).toContainText("Invalid email or password. Please try again.");
+  });
+
+  test("wrong password shows error", async ({ page }) => {
+    await page.click(LOGIN_BTN);
+    await page.fill(EMAIL_IN, process.env.TEST_USER_EMAIL);
+    await page.fill(PASS_IN, "wrong-password");
+    await page.click(SUBMIT_BTN);
+    await expect(page.locator(MSG_CONT)).toBeVisible();
+    await expect(page.locator(MSG_CONT)).toContainText("Invalid email or password. Please try again.");
   });
 });
